547f0f71c2ad4d559924eb3a7b9b1a4cef0cc005
mkouzios <mkouzios@hotmail.com>
2020-01-25 22:41:04 -0600

    Progress on interpreting commands as linux commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 var logger = require('winston');
 var auth = require('./auth.json');
+const { exec,spawn } = require('child_process');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -11,13 +12,26 @@ logger.level = 'debug';
 
 const bot = new TelegramBot(auth.token, {polling: true});
 
+const options = {
+  parse_mode: 'html'
+}
+
 bot.onText(/\/man (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  const response = "Unable to find that man page!"
+  const page = match[1]; // the captured page
+
+  logger.info(msg.from.first_name + " " + msg.from.last_name + " requested a man call for " + page)
 
   // Linux magic
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, response);
+  exec('man ' + page, (err, stdout, stderr) => {
+    if (stderr) {
+      console.log(stderr)
+      logger.error(stderr)
+      bot.sendMessage(chatId, "<b>Your command received the following error:</b>\n<code>" + stderr + "</code>", options);
+    } else {
+      console.log(stdout)
+      logger.info(msg.from.first_name + " " + msg.from.last_name + " received a man call for " + page)
+      bot.sendMessage(chatId, stdout, options);
+    }
+  });
 });
